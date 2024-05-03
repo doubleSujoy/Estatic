5b7dd8e2a39d5239438d9fccce304d1abb754107
doubleSujoy <166734633+doubleSujoy@users.noreply.github.com>
2024-05-03 20:47:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,16 @@ def sendPing():
 def jdk():
     try:
         if not request.is_json:
-            return jsonify({"serverError": False, "clientError": True, "msg": "use application/jaon in request header to complete the request"})
-        data = request.json  # Assuming JSON data is sent in the request body
+            return jsonify({"serverError": False, "clientError": True, "msg": "use application/json in request header to complete the request"})
+
+        try:
+            if not request.json:
+                return return jsonify({"serverError": False, "clientError": True, "msg": "A valid json request body is not provided!"})
+         
+            data = request.json  # Assuming JSON data is sent in the request body
+        except Exception as e:
+            return jsonify({"serverError": False, "clientError": True, "msg": "A valid json request body is not provided!"})
+        
         q = data.get('query')  # Assuming 'port_number' is the key in the JSON data
         if q is None:
             return jsonify({"serverError": False, "clientError": True, "msg": "query is not provided in request body!"})
