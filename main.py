b4196a9096823ffb5c4c8db4044151fb39ea052c
doubleSujoy <166734633+doubleSujoy@users.noreply.github.com>
2024-05-03 13:13:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ app = Flask(__name__)
 def home():
     return "Hello, this is the homepage!"
 
+@app.route("/ping")
+def pingmeio():
+    return True
+
+@app.route("/test")
+def jdkrk():
+    print(fun832())
+    print(fun183())
+    return True
+
+
 if __name__ == '__main__':
     # Run the app
     app.run(debug=True,host='0.0.0.0',port=3000)
