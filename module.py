23a4d90eddcdf98035ffe547fa9e5e07aac0adff
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-05-04 13:47:36 +0530

    Update module.py

--- module.py
+++ module.py
@@ -5,24 +5,24 @@ ChatGptClient = Client(os.environ.get('infiChatModel', None), hf_token = hf_tok)
 tool_sphere_code_model = Client("crystal99/tool-sphere-code-gen", hf_token = hf_tok)
 
 def giiyel7hosx(q):
-  result = ChatGptClient.predict(
+      result = ChatGptClient.predict(
 		message=f"{q}",
 		request="You are a friendly Chatbot.",
 		param_3=512,
 		param_4=0.7,
 		param_5=0.95,
 		api_name="/chat"
-)
-  return result
+      )
+      return result
 
 def bosohwf84kv(q):
-  result = tool_sphere_code_model.predict(
+      result = tool_sphere_code_model.predict(
 		message=f"{q}",
 		request="You are a friendly Chatbot.",
 		param_3=2100,
 		param_4=0.7,
 		param_5=0.95,
 		api_name="/chat"
-)
-  return result
+      )
+      return result
 
