c0667200d391e4def6190a1f2fe298fb25ffcce1
doubleSujoy <166734633+doubleSujoy@users.noreply.github.com>
2024-05-10 15:08:47 +0530

    Update module.py

--- module.py
+++ module.py
@@ -38,7 +38,7 @@ def imgGenv1(q):
 		param_0=f"{q}",
 		api_name="/predict"
     )
-    response = requests.get(f"https://crystal99-runwayml-stable-diffusion-v1-5.hf.space/file=${result3}")
+    response = requests.get(f"https://crystal99-runwayml-stable-diffusion-v1-5.hf.space/file={result3}")
     if response.status_code == 200:
 	    base64_image = base64.b64encode(response.content)
 	    base64_image_str = base64_image.decode('utf-8')
