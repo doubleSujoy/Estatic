ef945d91e5e70a99f4769c6e99d4508dbbba5c3d
doubleSujoy <166734633+doubleSujoy@users.noreply.github.com>
2024-05-10 15:29:17 +0530

    [skip render]

--- module.py
+++ module.py
@@ -49,10 +49,29 @@ def imgGenv1(q):
 
 
 def imgGenDetai(q):
-	result = "some"
-	return result
+    result4 = imageGenVone.predict(
+		param_0=f"{q}",
+		api_name="/predict"
+    )
+    response = requests.get(f"https://crystal99-runwayml-stable-diffusion-v1-5.hf.space/file={result4}")
+    if response.status_code == 200:
+	    base64_image = base64.b64encode(response.content)
+	    base64_image_str = base64_image.decode('utf-8')
+	    return f"data:image/jpeg;base64,{base64_image_str}"
+    else:
+	    return "Failed to fetch Image"
+
 
 def imageGenPix(q):
-	result = "some"
-	return result
-
+    result5 = imageGenVone.predict(
+		param_0=f"{q}",
+		api_name="/predict"
+    )
+    response = requests.get(f"https://crystal99-runwayml-stable-diffusion-v1-5.hf.space/file={result5}")
+    if response.status_code == 200:
+	    base64_image = base64.b64encode(response.content)
+	    base64_image_str = base64_image.decode('utf-8')
+	    return f"data:image/jpeg;base64,{base64_image_str}"
+    else:
+	    return "Failed to fetch Image"
+	    
